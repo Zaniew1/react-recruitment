dfbb363ff599f466c6d9d3f3838bd49bb49ec578
Mateusz Zaniewski <67514879+Zaniew1@users.noreply.github.com>
2024-04-04 17:17:28 +0200

    Update Table.stories.tsx
    add another story

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -18,3 +18,21 @@ Default.args = {
     // Add more rows as needed
   ],
 };
+export const Multiple = Template.bind({});
+Multiple.args = {
+  rows: [
+    { id: 1, name: 'Tag 1', count: 10, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 2, name: 'Tag 2', count: 20, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 3, name: 'Tag 3', count: 30, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 4, name: 'Tag 123', count: 10, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 5, name: 'Tag 223', count: 20, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 6, name: 'Tag 324', count: 30, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 7, name: 'Tag 155', count: 10, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 8, name: 'Tag 222', count: 20, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 9, name: 'Tag 311', count: 30, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 10, name: 'Tag 166', count: 10, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 11, name: 'Tag 277', count: 20, has_synonyms: false, is_moderator_only: false, is_required: true },
+    { id: 12, name: 'Tag 32', count: 30, has_synonyms: false, is_moderator_only: false, is_required: true },
+    // Add more rows as needed
+  ],
+};
